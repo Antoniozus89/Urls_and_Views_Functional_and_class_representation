0ba673f29809a8db16a57278a3bcda7701d46765
Antoniozus89 <166949414+Antoniozus89@users.noreply.github.com>
2024-10-06 12:07:27 +0300

    Add files via upload

---
+++ UrbanDjango/task2/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from task2.views import ClassView, func_view
+
+# urlpatterns = [
+    # path('class_view/',ClassView.as_view() ),
+    # path('function_view/',func_view),
+# ]
